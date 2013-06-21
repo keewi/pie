11417f4fad0e66739cbd6cc4121a5740e4de7e03
Emily Yeh <xemily.yeh@gmail.com>
2013-06-21 13:51:57 -0700

    intro

---
+++ 01_display.py
@@ -0,0 +1,24 @@
+#You can make comments in code! These comments are like sticky-notes. They don't execute.
+
+# You can comment
+# multiple lines
+# in Sublime
+# by selecting the area,
+# then pressing Ctrl+/
+#And do the same thing to unselect an area. try to uncomment the next line:
+# x = 1
+#Variable x is set to an int, 1.
+
+#PROJECT 1: Set a variable for your birthday (int) and for your name (string).
+
+#PROJECT 2: Print your name.
+
+#PROJECT 3: Print "My name is [your name]. I am [age] years old."
+
+
+
+
+#HINTS:
+# Project 1) Google declaring ints and strings
+# 2) Google printing strings
+# 3) Google printing multiple items
